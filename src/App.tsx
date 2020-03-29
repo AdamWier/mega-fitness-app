1a16765b0667ed21de69193018782c19114186b8
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-03-29 12:12:33 +0200

    Refactored stack navigator. Closes #04

--- src/App.tsx
+++ src/App.tsx
@@ -6,36 +6,12 @@ import {theme, navTheme} from './StyleSheet';
 import Search from './screens/Search';
 import Details from './screens/Details';
 import Day from './screens/Day';
-
-const Stack = createStackNavigator();
+import Navigation from './Navigation';
 
 function App() {
-  const [meal, updateMeal] = React.useState([]);
-
   return (
     <ThemeProvider theme={theme}>
-      <NavigationContainer theme={navTheme}>
-        <Stack.Navigator screenOptions={{
-          headerStyle: {
-            backgroundColor: '#375a7f',
-          },
-          headerTitleStyle: {
-            color: '#fff',
-          },
-          headerTintColor: 'rgba(255, 255, 255, 0.6)',
-        }}>
-          <Stack.Screen name='Day'>
-            {props => <Day {...props} meal={meal} />}
-          </Stack.Screen>
-          <Stack.Screen name='Search' component={Search} />
-          <Stack.Screen name='Details' 
-            options={({ route }) => ({ 
-              title: route.params.details.name, 
-            })}>
-            {props => <Details {...props} meal={meal} updateMeal={updateMeal} />}
-          </Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Navigation/>
     </ThemeProvider>
   );
 }
---
+++ src/Navigation/Screens.ts
@@ -0,0 +1,21 @@
+import Day from "../screens/Day";
+import Search from "../screens/Search";
+import Details from "../screens/Details";
+
+export default [
+    {
+        name: "Day",
+        component: Day,
+        props: true,
+    },{
+        name: "Search",
+        component: Search,
+    },{
+        name: "Details",
+        component: Details,
+        props: true,
+        options: ({ route }) => ({ 
+            title: route.params.details.name, 
+        })
+    }
+]
