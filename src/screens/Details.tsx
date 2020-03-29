4657d7564f8828d4b9bd20ebc9287c0255f64c16
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-03-29 11:05:25 +0200

    Added calculateNutrient function. Closes #06

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -7,13 +7,17 @@ import FoodCard from '../components/FoodCard';
 
 export default function Details({ navigation, route, meal, updateMeal }) {
 
+  const calculateNutrient = (nutrient) => {
+    return Math.round(details[nutrient] * amount * currentPortion.weight);
+  }
+
   const details: USDAFoodDetails = route.params.details;
 
   const calculateValues = () => {
-    const calories = Math.round(details.calories * amount * currentPortion.weight);
-    const protein = Math.round(details.protein * amount * currentPortion.weight);
-    const carbs = Math.round(details.carbs * amount * currentPortion.weight);
-    const fats = Math.round(details.fats * amount * currentPortion.weight);
+    const calories = calculateNutrient("calories");
+    const protein = calculateNutrient("protein");
+    const carbs = calculateNutrient("carbs");
+    const fats = calculateNutrient("fats");
     return {
       calories,
       protein,
@@ -53,29 +57,29 @@ export default function Details({ navigation, route, meal, updateMeal }) {
         carbs={currentCalculations.carbs.toString()} 
         fats={currentCalculations.fats.toString()} 
       >
-      <AmountPicker 
-            amounts={details.portions} 
-            selectedValue={currentPortion.description} 
-            onValueChange={
-              (selection) => {
-                const newPortion = details.portions.find(portion => 
-                  selection === portion.description
-                );
-                changePortion(newPortion);
-              }
-            } 
-          />
-          <Slider 
-             step={1} 
-             minimumValue={1} 
-             maximumValue={1000} 
-             value={amount} 
-             onValueChange={value => changeAmount(value)}
-           />
-           </FoodCard>
-       <Button title="Add food" onPress={() => {
-         addFood();
-       }} />
-     </ScrollView>
+        <AmountPicker 
+          amounts={details.portions} 
+          selectedValue={currentPortion.description} 
+          onValueChange={
+            (selection) => {
+              const newPortion = details.portions.find(portion => 
+                selection === portion.description
+              );
+              changePortion(newPortion);
+            }
+          } 
+        />
+        <Slider 
+          step={1} 
+          minimumValue={1} 
+          maximumValue={1000} 
+          value={amount} 
+          onValueChange={value => changeAmount(value)}
+        />
+      </FoodCard>
+      <Button title="Add food" onPress={() => {
+        addFood();
+      }} />
+    </ScrollView>
   );
 }
