5719748689471ad7ff27a9513df4534e89c45cca
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-04-03 17:08:20 +0200

    Added state to screen. Closes #13

--- src/screens/AccountCreation.tsx
+++ src/screens/AccountCreation.tsx
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, Input, Button } from 'react-native-elements';
 import { View, StyleSheet } from 'react-native';
 
 export default function AccountCreation(): JSX.Element {
+
+  const [signUpDetails, setSignUpDetails] = useState({
+    username: '',
+    password: '',
+    passwordConfirmation: '',
+  })
+
+  const updateSignUpDetails = (value: string, field: string): void => {
+    setSignUpDetails(state =>({
+      ...state,
+      [field]: value,
+    }));
+  };
+
+  const createAcount = (): any => {
+    console.log(signUpDetails);
+  };
+
   return (
     <View style={style.content}>
       <Text>Create an account</Text>
-      <Input placeholder="Username" />
-      <Input placeholder="Password" />
-      <Input placeholder="Confirm password" />
-      <Button title="Create account" />
+      <Input 
+        placeholder="Username" 
+        onChangeText={
+          (value): void => updateSignUpDetails(value, 'username')
+        }
+      />
+      <Input 
+        placeholder="Password" 
+        secureTextEntry
+        onChangeText={
+          (value): void => updateSignUpDetails(value, 'password')
+        }
+      />
+      <Input 
+        placeholder="Confirm password" 
+        secureTextEntry 
+        onChangeText={
+          (value): void => updateSignUpDetails(value, 'passwordConfirmation')
+        }
+      />
+      <Button 
+        title="Create account" 
+        onPress={createAcount}
+        />
     </View>
   );
 }
