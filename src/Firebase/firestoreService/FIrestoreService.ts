f5e24afa78a1619185be9695d038b51003b1a851
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-04-18 13:58:07 +0200

    Fixed issue with meal name not saving right at creation. Closes #22

--- src/Firebase/firestoreService/FIrestoreService.ts
+++ src/Firebase/firestoreService/FIrestoreService.ts
@@ -1,6 +1,6 @@
 export default interface FirestoreService {
-    createMeal(meal: Array<any>, mealName: string, uid: string, eatenAt: Date): Promise<void>;
-    updateMeal(meal: Array<any>, mealName: string, uid: string, eatenAt: Date, id: string): Promise<void>;
+    createMeal(meal: Array<any>, name: string, uid: string, eatenAt: Date): Promise<void>;
+    updateMeal(meal: Array<any>, name: string, uid: string, eatenAt: Date, id: string): Promise<void>;
     deleteMeal(id: string): Promise<void>;
     findMealsByDate(dateStart: Date, uid: string): Promise<Array<any>>;
     saveUser(user: {uid: string, email: string}): Promise<void>;
--- src/Firebase/firestoreService/FirestoreServiceImpl.ts
+++ src/Firebase/firestoreService/FirestoreServiceImpl.ts
@@ -10,15 +10,15 @@ export default class FirestoreServiceImpl implements FirestoreService {
     this.firestore = firestore;
   }
 
-  createMeal(meal: Array<any>, mealName: string, uid: string, eatenAt: Date): Promise<void> {
+  createMeal(meal: Array<any>, name: string, uid: string, eatenAt: Date): Promise<void> {
     const createdAt = new Date();
-    return this.firestore.collection('meals').doc(uid+'-'+createdAt.getTime()+'-'+mealName).set({
+    return this.firestore.collection('meals').doc(uid+'-'+createdAt.getTime()+'-'+name).set({
       meal,
       eatenAt,
       createdAt,
       deleted: false,
       uid,
-      mealName,
+      name,
     });
   }
 
