3ddc8647865299f39c77df93db4a8df15aef42f7
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-06-01 23:19:47 +0200

    Refactored GoalPrompt into own component. Closes #56

---
+++ src/components/GoalPrompt.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Text, Button, Input } from 'react-native-elements';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import PropTypes from 'prop-types';
+
+const GoalPrompt = ({
+  toggleIsOverlayVisible,
+  goalCalories,
+  setGoalCalories,
+  onConfirmButtonPress,
+  loading,
+}) => (
+  <View style={styles.overlayContentContainer}>
+    <Text h3>Let's set a goal!</Text>
+    <Text h4>How many calories for today?</Text>
+    <Input
+      containerStyle={styles.inputContainer}
+      value={goalCalories}
+      onChangeText={(value) => setGoalCalories(value)}
+      keyboardType="number-pad"
+    />
+    {loading ? (
+      <ActivityIndicator size="large" />
+    ) : (
+      <View style={styles.buttonContainer}>
+        <Button title="Confirm" onPress={onConfirmButtonPress} />
+        <Button title="Cancel" onPress={() => toggleIsOverlayVisible(false)} />
+      </View>
+    )}
+  </View>
+);
+
+GoalPrompt.propTypes = {
+  toggleIsOverlayVisible: PropTypes.func.isRequired,
+  goalCalories: PropTypes.string.isRequired,
+  setGoalCalories: PropTypes.func.isRequired,
+  onConfirmButtonPress: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+
+const styles = StyleSheet.create({
+  overlayContentContainer: {
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    marginVertical: 0,
+    marginHorizontal: 0,
+    width: 200,
+    padding: 0,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+});
+
+export default GoalPrompt;
--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
-import { Text, Input, Button } from 'react-native-elements';
+import { Text } from 'react-native-elements';
 import { View, StyleSheet, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
-import { authService } from '../Firebase';
 import { container } from '../store/reducers/User';
+import GoalPrompt from '../components/GoalPrompt';
 
-function Settings({ user, navigation, storeLogin }): JSX.Element {
+function Settings({ user, storeLogin }): JSX.Element {
+  const [goalCalories, setGoalCalories] = useState('0');
+
+  const saveCalories = () => console.log(null);
+
   return (
     <View style={style.content}>
-      <Text h2>User Settings For</Text>
-      <Text h4>{user.email}</Text>
+      <View style={style.equalSpace}>
+        <Text h2>User Settings For</Text>
+        <Text h4>{user.email}</Text>
+      </View>
+      <View style={style.equalSpace}>
+        <GoalPrompt 
+          goalCalories={goalCalories} 
+          setGoalCalories={setGoalCalories} 
+          onConfirmButtonPress={saveCalories} 
+          loading={false} 
+        />
+      </View>
+      <View style={style.equalSpace}/>
     </View>
   );
 }
@@ -17,14 +32,17 @@ function Settings({ user, navigation, storeLogin }): JSX.Element {
 const style = StyleSheet.create({
   content: {
     paddingTop: StatusBar.currentHeight,
+    justifyContent: 'space-evenly',
+    flex: 1
   },
+  equalSpace: {
+    flex: 1
+  }
 });
 
-// Settings.propTypes = {
-//   navigation: PropTypes.shape({
-//     navigate: PropTypes.func.isRequired,
-//   }).isRequired,
-//   storeLogin: PropTypes.func.isRequired,
-// };
+Settings.propTypes = {
+  user: PropTypes.shape({ uid: PropTypes.string, email: PropTypes.string }).isRequired,
+  storeLogin: PropTypes.func.isRequired,
+};
 
 export default container(Settings);
