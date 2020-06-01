30599ee5e5142738aafa594f46e5558cb2f0951d
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-06-01 20:10:48 +0200

    Unrendered barcode scanner when not in view. Closes #81

--- src/screens/BarCodeScanner.tsx
+++ src/screens/BarCodeScanner.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import Toast from 'react-native-simple-toast';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import OFDApiImpl from '../ApiHelpers/OFD/OFDApiImpl';
 import PropTypes from 'prop-types';
+import { useFocusEffect } from '@react-navigation/native';
 
 export default function BarCodeScannerScreen({ navigation }) {
+  const [isFocused, toggleIsFocused] = useState(false);
+
   const handleBarCodeScanned = async ({ data }) => {
     try {
       const OFDApi = new OFDApiImpl();
@@ -30,13 +33,18 @@ export default function BarCodeScannerScreen({ navigation }) {
     }
   };
 
-  return (
+  useFocusEffect(() => {
+    toggleIsFocused(true);
+    return () => toggleIsFocused(false);
+  });
+
+  return isFocused ? (
     <BarCodeScanner
       barCodeTypes={[BarCodeScanner.Constants.BarCodeType.interleaved2of5]}
       onBarCodeScanned={handleBarCodeScanned}
       style={StyleSheet.absoluteFillObject}
     />
-  );
+  ) : null;
 }
 
 BarCodeScannerScreen.propTypes = {
