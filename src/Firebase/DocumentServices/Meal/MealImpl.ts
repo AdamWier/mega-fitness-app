ce7e333ca019c46d910840b0abc2488d4dc02fe2
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-06-14 17:22:32 +0200

    Added averages to weekly report. Closes #51

--- src/Firebase/DocumentServices/Meal/MealImpl.ts
+++ src/Firebase/DocumentServices/Meal/MealImpl.ts
@@ -93,6 +93,32 @@ export default class MealImpl implements Meal {
       .where('deleted', '==', false);
   }
 
+  async findByWeek(
+    beginngingOfWeek: Date,
+    uid: string
+  ): Promise<{ [key: string]: any }[]> {
+    const response = await this.getByWeekRef(beginngingOfWeek, uid).get();
+    if (response.docs.length) {
+      return response.docs.map(this.mapDocuments);
+    }
+    return [];
+  }
+
+  getByWeekRef(
+    beginngingOfWeek: Date,
+    uid: string
+  ): firebase.firestore.Query<firebase.firestore.DocumentData> {
+    const start = moment(beginngingOfWeek).startOf('week');
+    const end = start.clone().endOf('week');
+    console.log({ start, end });
+    return this.firestore
+      .collection('meals')
+      .where('eatenAt', '>=', start.toDate())
+      .where('eatenAt', '<', end.toDate())
+      .where('uid', '==', uid)
+      .where('deleted', '==', false);
+  }
+
   mapDocuments(
     document: firebase.firestore.QueryDocumentSnapshot<
       firebase.firestore.DocumentData
--- src/screens/WeeklyReport.tsx
+++ src/screens/WeeklyReport.tsx
@@ -5,27 +5,83 @@ import PropTypes from 'prop-types';
 import { container } from '../store/reducers/User';
 import CustomHeader from '../components/Header';
 import moment from 'moment';
-import {CalendarList} from 'react-native-calendars';
+import { CalendarList } from 'react-native-calendars';
 import { withTheme } from 'react-native-elements';
+import { mealDocumentService } from '../Firebase/index';
+import FoodCard from '../components/FoodCard';
+
+const createMealReport = (mealDocuments: { [key: string]: any }) => {
+  const meals = mealDocuments.flatMap(
+    (document: { [key: string]: any }) => document.meal
+  );
+
+  return meals.reduce(
+    (
+      accum: { [key: string]: number },
+      next: { [key: string]: number },
+      index: number
+    ) => {
+      accum.calories += next.calories;
+      accum.protein += next.protein;
+      accum.carbs += next.carbs;
+      accum.fats += next.fats;
+      if (index === meals.length - 1) {
+        accum.calories /= meals.length;
+        accum.protein /= meals.length;
+        accum.carbs /= meals.length;
+        accum.fats /= meals.length;
+      }
+      return accum;
+    },
+    { calories: 0, protein: 0, carbs: 0, fats: 0 }
+  );
+};
 
 function WeeklyReport({ user, theme }): JSX.Element {
-  const [period, setPeriod] = useState({})
+  const [period, setPeriod] = useState({});
+  const [report, setReport] = useState({
+    calories: 0,
+    protein: 0,
+    fats: 0,
+    carbs: 0,
+  });
 
-  const onDayPress = (date) => {
+  const onDayPress = async (date: { [key: string]: any }) => {
     const currentMoment = moment(date.dateString);
     const beginningOfWeek = moment(currentMoment.startOf('isoWeek'));
     const week = {
-      [beginningOfWeek.format('YYYY-MM-DD')]: {startingDay: true, color: theme.colors.success},
-      [beginningOfWeek.clone().add('1', 'day').format('YYYY-MM-DD')]: {color: theme.colors.success},
-      [beginningOfWeek.clone().add('2', 'day').format('YYYY-MM-DD')]: {color: theme.colors.success},
-      [beginningOfWeek.clone().add('3', 'day').format('YYYY-MM-DD')]: {color: theme.colors.success},
-      [beginningOfWeek.clone().add('4', 'day').format('YYYY-MM-DD')]: {color: theme.colors.success},
-      [beginningOfWeek.clone().add('5', 'day').format('YYYY-MM-DD')]: {color: theme.colors.success},
-      [beginningOfWeek.clone().add('6', 'day').format('YYYY-MM-DD')]: {endingDay: true, color: theme.colors.success}
-    }
+      [beginningOfWeek.format('YYYY-MM-DD')]: {
+        startingDay: true,
+        color: theme.colors.success,
+      },
+      [beginningOfWeek.clone().add('1', 'day').format('YYYY-MM-DD')]: {
+        color: theme.colors.success,
+      },
+      [beginningOfWeek.clone().add('2', 'day').format('YYYY-MM-DD')]: {
+        color: theme.colors.success,
+      },
+      [beginningOfWeek.clone().add('3', 'day').format('YYYY-MM-DD')]: {
+        color: theme.colors.success,
+      },
+      [beginningOfWeek.clone().add('4', 'day').format('YYYY-MM-DD')]: {
+        color: theme.colors.success,
+      },
+      [beginningOfWeek.clone().add('5', 'day').format('YYYY-MM-DD')]: {
+        color: theme.colors.success,
+      },
+      [beginningOfWeek.clone().add('6', 'day').format('YYYY-MM-DD')]: {
+        endingDay: true,
+        color: theme.colors.success,
+      },
+    };
     setPeriod(week);
-  }
-
+    const mealDocuments = await mealDocumentService.findByWeek(
+      beginningOfWeek.toDate(),
+      user.uid
+    );
+    const generatedReport = createMealReport(mealDocuments);
+    setReport(generatedReport);
+  };
 
   return (
     <View style={style.content}>
@@ -34,7 +90,7 @@ function WeeklyReport({ user, theme }): JSX.Element {
         <Text h2>Select a week</Text>
         <CalendarList
           markedDates={period}
-          markingType={"period"}
+          markingType={'period'}
           onDayPress={onDayPress}
           firstDay={1}
           hideDayNames
@@ -47,6 +103,18 @@ function WeeklyReport({ user, theme }): JSX.Element {
         />
       </View>
       <View style={style.equalSpace}>
+        {report.calories ? (
+          <View>
+            <FoodCard
+              name="Weekly Average"
+              calories={report.calories.toString()}
+              protein={report.protein.toString()}
+              fats={report.fats.toString()}
+              carbs={report.carbs.toString()}
+              expanded
+            />
+          </View>
+        ) : null}
       </View>
       <View style={style.equalSpace} />
     </View>
