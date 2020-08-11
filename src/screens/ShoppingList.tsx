4e3a4987176cab9f84d17bd1fdc963547efee6e9
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-08-11 20:09:32 +0200

    Created shopping list display. Closes #96

--- src/screens/ShoppingList.tsx
+++ src/screens/ShoppingList.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text } from 'react-native-elements';
+import { Text, Card } from 'react-native-elements';
 import { View, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import { container } from '../store/reducers/User';
@@ -8,10 +8,17 @@ import { ScrollView } from 'react-native-gesture-handler';
 import WeekSelector from '../components/WeekSelector';
 import { mealDocumentService } from '../Firebase';
 import moment from 'moment';
+import UpDownButtons from '../components/UpDownButtons';
 
 function ShoppingList({ user }): JSX.Element {
   const [period, setPeriod] = useState({});
   const [list, setList] = useState([]);
+
+  const updateList = (food: string, portion: string, updatedNumber: string) => {
+    const newFood = list[food];
+    newFood[portion] = updatedNumber;
+    setList((list) => ({ ...list, [food]: newFood }));
+  };
 
   useEffect(() => {
     (async function generateList() {
@@ -44,13 +51,28 @@ function ShoppingList({ user }): JSX.Element {
         <WeekSelector period={period} setPeriod={setPeriod} />
       </View>
       <ScrollView style={style.listSpace}>
-        {Object.keys(list).map((food) =>
-          Object.keys(list[food]).map((portion) => (
-            <Text
-              key={food + portion}
-            >{`${food}: ${portion} ${list[food][portion]}`}</Text>
-          ))
-        )}
+        <Card title="Shopping list">
+          {Object.keys(list).map((food) =>
+            Object.keys(list[food]).map((portion) => (
+              <View>
+                <Text key={food + portion} style={style.listItem}>
+                  {food}
+                </Text>
+                <View style={style.subItem}>
+                  <UpDownButtons
+                    total={list[food][portion]}
+                    onValueChange={(updatedNumber: string) =>
+                      updateList(food, portion, updatedNumber)
+                    }
+                  />
+                  <Text>
+                    {list[food][portion]} {portion}
+                  </Text>
+                </View>
+              </View>
+            ))
+          )}
+        </Card>
       </ScrollView>
     </View>
   );
@@ -63,8 +85,17 @@ const style = StyleSheet.create({
   listSpace: {
     flex: 1,
   },
+  listItem: {
+    textAlign: 'left',
+    fontSize: 20,
+  },
+  subItem: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
   calendarContainer: {
-    flex: 1,
+    flex: 0.5,
   },
 });
 
