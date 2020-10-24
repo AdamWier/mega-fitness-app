ab88c6755dffcc801c8c399c7b6aa03e52529da2
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-10-24 11:41:14 +0200

    Add test for search by ID. Closes #53

--- tests/ApiHelpers/OFD/OFDApi.ts
+++ tests/ApiHelpers/OFD/OFDApi.ts
@@ -2,6 +2,7 @@ import OFDApi from '../../../src/ApiHelpers/OFD/OFDApi';
 import fetchMock from 'jest-fetch-mock';
 import OFDSearchResult from '../../Fixtures/OFDSearchResponse.json';
 import OFDBarcodeSearchResult from '../../Fixtures/OFDBarcodeSearchResult.json';
+import OFDDetailsByIdResult from '../../Fixtures/OFDDetailsById.json';
 
 describe('open Food Data Api', () => {
   fetchMock.enableMocks();
@@ -38,6 +39,32 @@ describe('open Food Data Api', () => {
     fetchMock.mockResponseOnce(JSON.stringify(OFDBarcodeSearchResult));
 
     const results = await api.barcodeSearch('5000159344074');
+
+    expect(results).toStrictEqual(
+      expect.objectContaining({
+        name: expect.any(String),
+        calories: expect.any(Number),
+        protein: expect.any(Number),
+        fats: expect.any(Number),
+        carbs: expect.any(Number),
+        portions: expect.arrayContaining([
+          expect.objectContaining({
+            description: expect.any(String),
+            weight: expect.any(Number),
+          }),
+        ]),
+      })
+    );
+  });
+
+  it('correctly gets the details by id from an OFD text search', async () => {
+    expect.assertions(1);
+
+    const api = new OFDApi();
+
+    fetchMock.mockResponseOnce(JSON.stringify(OFDDetailsByIdResult));
+
+    const results = await api.getDetails('0076840600021');
 
     expect(results).toStrictEqual(
       expect.objectContaining({
