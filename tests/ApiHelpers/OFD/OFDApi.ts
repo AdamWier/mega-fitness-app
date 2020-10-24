7f275b85a7863e44f622ab057b837aaf23036ee1
AdamWier <38018787+AdamWier@users.noreply.github.com>
2020-10-24 11:30:16 +0200

    Add barcode search test. Closes #53

--- tests/ApiHelpers/OFD/OFDApi.ts
+++ tests/ApiHelpers/OFD/OFDApi.ts
@@ -3,7 +3,7 @@ import fetchMock from 'jest-fetch-mock';
 import OFDSearchResult from '../../Fixtures/OFDSearchResponse.json';
 import OFDBarcodeSearchResult from '../../Fixtures/OFDBarcodeSearchResult.json';
 
-describe('Open Food Data Api', () => {
+describe('open Food Data Api', () => {
   fetchMock.enableMocks();
 
   beforeEach(() => {
@@ -27,6 +27,32 @@ describe('Open Food Data Api', () => {
           api: 'Open Food Data',
         }),
       ])
+    );
+  });
+
+  it('correctly formats results from an OFD barcode search', async () => {
+    expect.assertions(1);
+
+    const api = new OFDApi();
+
+    fetchMock.mockResponseOnce(JSON.stringify(OFDBarcodeSearchResult));
+
+    const results = await api.barcodeSearch('5000159344074');
+
+    expect(results).toStrictEqual(
+      expect.objectContaining({
+        name: expect.any(String),
+        calories: expect.any(Number),
+        protein: expect.any(Number),
+        fats: expect.any(Number),
+        carbs: expect.any(Number),
+        portions: expect.arrayContaining([
+          expect.objectContaining({
+            description: expect.any(String),
+            weight: expect.any(Number),
+          }),
+        ]),
+      })
     );
   });
 });
