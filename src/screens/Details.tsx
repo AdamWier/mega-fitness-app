7a701056c688be6bbab016587c9f615c495e1893
AdamWier <38018787+AdamWier@users.noreply.github.com>
2022-10-22 21:46:37 +0200

    Calculate other nutriments, make sure nutriments are saved as numbers, make sure calories are calculated on first load

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'react-native-elements';
 import { ScrollView, Alert } from 'react-native';
 import AmountPicker from '../components/AmountPicker';
@@ -12,6 +12,7 @@ import {
 } from '../Navigation/FoodJournalStack/Screens';
 import { ConnectedProps } from 'react-redux';
 import { RouteProp } from '@react-navigation/native';
+import { useCallback } from 'react';
 
 function Details({
   navigation,
@@ -21,23 +22,29 @@ function Details({
 }: DetailsProps & ConnectedProps<typeof container>) {
   const { details } = route.params;
 
-  const [amount, setAmount] = useState('1');
-  const [currentPortion, changePortion] = useState(details.portions[0]);
-  const [calories, setCalories] = useState('0');
-  const [protein, setProtein] = useState('0');
-  const [carbs, setCarbs] = useState('0');
-  const [fats, setFats] = useState('0');
-
   const calculateNutrient = useCallback(
-    (nutrient: keyof FoodDetails, amount: string) =>
+    (
+      nutrient: keyof FoodDetails,
+      amount: string,
+      currentPortionWeight: number
+    ) =>
       (Number(amount)
         ? Math.round(
-            Number(details[nutrient]) * Number(amount) * currentPortion.weight
+            Number(details[nutrient]) * Number(amount) * currentPortionWeight
           )
         : 0
       ).toString(),
-    [currentPortion.weight, details]
+    [details]
   );
+
+  const [amount, setAmount] = useState('1');
+  const [currentPortion, changePortion] = useState(details.portions[0]);
+  const [calories, setCalories] = useState(
+    calculateNutrient('calories', amount, currentPortion.weight)
+  );
+  const [protein, setProtein] = useState('0');
+  const [carbs, setCarbs] = useState('0');
+  const [fats, setFats] = useState('0');
 
   const calculateAmountFromCalories = (caloriesAsString: string) => {
     const caloriesAsNumber = Number(caloriesAsString);
@@ -47,9 +54,13 @@ function Details({
       ) / 100
     ).toString();
   };
-  const calculateCaloriesByAmount = (value: string) => {
+
+  const calculateCaloriesByAmount = (
+    value: string,
+    currentPortionWeight: number
+  ) => {
     setAmount(value);
-    setCalories(calculateNutrient('calories', value));
+    setCalories(calculateNutrient('calories', value, currentPortionWeight));
   };
 
   const calculateAmountByCalories = (value: string) => {
@@ -57,11 +68,28 @@ function Details({
     setCalories(value);
   };
 
+  const onPortionChange = (selection: string): void => {
+    const newPortion = details.portions.find(
+      (portion: FormattedPortion) => selection === portion.description
+    );
+    if (newPortion) {
+      changePortion(newPortion);
+      calculateCaloriesByAmount(amount, newPortion.weight);
+    }
+  };
+
   useEffect(() => {
-    setProtein(calculateNutrient('protein', amount));
-    setCarbs(calculateNutrient('carbs', amount));
-    setFats(calculateNutrient('fats', amount));
-  }, [amount, calculateNutrient, setProtein, setCarbs, setFats]);
+    setProtein(calculateNutrient('protein', amount, currentPortion.weight));
+    setCarbs(calculateNutrient('carbs', amount, currentPortion.weight));
+    setFats(calculateNutrient('fats', amount, currentPortion.weight));
+  }, [
+    amount,
+    calculateNutrient,
+    setProtein,
+    setCarbs,
+    setFats,
+    currentPortion.weight,
+  ]);
 
   const amountIsCorrect = () => {
     return amount !== '' && amount !== '0' && !!Number(amount);
@@ -77,10 +105,10 @@ function Details({
             name: details.name,
             amount: Number(amount),
             portionDescription: currentPortion.description,
-            calories,
-            protein,
-            fats,
-            carbs,
+            calories: Number(calories),
+            protein: Number(protein),
+            fats: Number(fats),
+            carbs: Number(carbs),
           },
         ],
       });
@@ -100,19 +128,16 @@ function Details({
         fats={fats}
         amount={amount}
         amountDescription={currentPortion.description}
-        onAmountChange={calculateCaloriesByAmount}
+        onAmountChange={(value) =>
+          calculateCaloriesByAmount(value, currentPortion.weight)
+        }
         onCalorieChange={calculateAmountByCalories}
         expanded
       >
         <AmountPicker
           amounts={details.portions}
           selectedValue={currentPortion.description}
-          onValueChange={(selection: string): void => {
-            const newPortion = details.portions.find(
-              (portion: FormattedPortion) => selection === portion.description
-            );
-            newPortion && changePortion(newPortion);
-          }}
+          onValueChange={onPortionChange}
         />
       </FoodCard>
       <Button title="Add food" onPress={addFood} />
